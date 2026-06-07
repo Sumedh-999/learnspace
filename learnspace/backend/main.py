378639e86824f5015ff3de566c36f31f3f04bb22
Sumedh S Mahajan <42212467+Sumedh-999@users.noreply.github.com>
2026-06-07 17:38:19 -0400

    Update main.py

--- learnspace/backend/main.py
+++ learnspace/backend/main.py
@@ -18,11 +18,14 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173")
+origins = [
+    "http://localhost:5173",
+    "https://learnspace-sage.vercel.app",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["frontend_url"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
